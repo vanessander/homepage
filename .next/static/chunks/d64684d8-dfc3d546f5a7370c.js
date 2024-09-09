f8e1153bdbae6430490de58e933c485ffe00ba26
Vanessander De Matos Muinge <vanessander@Vanessanders-MacBook-Pro.local>
2024-09-09 14:07:40 +0200

    test2

---
+++ .next/static/chunks/d64684d8-dfc3d546f5a7370c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[556],{3854:function(c,b,a){a.d(b,{T7S:function(){return e}});var d=a(8357);function e(a){return(0,d.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M17 8l4 4m0 0l-4 4m4-4H3"}}]})(a)}}}])
