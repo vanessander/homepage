66c20181efbfe4a89db47b526ff2b7034d3b8b66
Vanessander De Matos Muinge <vanessander@Vanessanders-MacBook-Pro.local>
2024-09-09 14:01:30 +0200

    test

---
+++ components/Experiences/data/data.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+        id: 1,
+        year: "React.js",
+        job_title: "Javascript Frontend Framework",
+        company: "Frontend Development",
+        description: "Rich experience developing dynamic and scalable applications with react.js"
+    },
+    {
+        id: 2,
+        year: "Django",
+        job_title: "Python Backend Framework",
+        company: "Backend Development",
+        description: "Significant Experience Developing Dynamic applications with Django"
+    },
+    {
+        id: 3,
+        year: "Full Stack",
+        job_title: "Full-Stack Developer",
+        company: "Full stack Development",
+        description: "Experience with diving in all parts of an application. From the database, backend, rest_apis and styling"
+    },
+    {
+        id: 4,
+        year: "PostgreSQL, SQLite...",
+        job_title: "SQL",
+        company: "Databases",
+        description: "Experience with operating, managing and deploying robust databases."
+    },
+    {
+        id: 5,
+        year: "Matplotlib",
+        job_title: "Python library",
+        company: "Data Visualization",
+        description: "In depth experience with data visualization technologies"
+    },
+    {
+        id: 6,
+        year: "3D Bioprinting",
+        job_title: "Published research in the international journal IJFMR",
+        company: "Researcher",
+        description: "conduct independent research on my free time"
+    }
+];
+
+export default data;
---
+++ components/Works/data/data.js
@@ -0,0 +1,60 @@
+const data = [
+    {
+        id: 1,
+        image_url: "https://alcatraz-bucket-demo.s3.eu-north-1.amazonaws.com/p1.png",
+        work_title: "Smart Brain",
+        description: "An application that allows you to detect faces of submitted images through the power of artificial intelligence. The user just has to enter the image url links. The application handles registration, signin, user data and many more. It was built by react js in the frontend, Node js and express js in the backend and uses postgresql as its database.",
+        site: "https://face-recognition-frontend-hz3b.onrender.com/",
+        source: "https://github.com/vanessander/face-recognition-frontend"
+    },
+    {
+        id: 2,
+        image_url: "https://alcatraz-bucket-demo.s3.eu-north-1.amazonaws.com/alcatraz.png",
+        work_title: "Alcatraz",
+        description: "The official commercial site for Alcatraz. Still in development...",
+        site: "https://alcatraz-xndf.onrender.com/",
+        source: "https://github.com/vanessander/Alcatraz"
+    },
+    {
+        id: 3,
+        image_url: "https://alcatraz-bucket-demo.s3.eu-north-1.amazonaws.com/meeting-notes.png",
+        work_title: "Meeting Notes",
+        description: "Convert the audio and voice of a meeting into written text easily! A Web application that converts audio into written text. Still in development",
+        site: "https://vanessander.github.io/meeting-notes/",
+        source: "https://github.com/vanessander/meeting-notes"
+    },
+    {
+        id: 4,
+        image_url: "https://alcatraz-bucket-demo.s3.eu-north-1.amazonaws.com/keiko-corp.png",
+        work_title: "Keiko Corp",
+        description: "A Leading tech start up website named Keiko Corp.<br> made for the Hacktoberfest 2020 challenge",
+        site: "https://keikocorp.netlify.app",
+        source: "https://github.com/vanessander/Keiko-Corp"
+    },
+    {
+        id: 5,
+        image_url: "https://alcatraz-bucket-demo.s3.eu-north-1.amazonaws.com/robofriends.png",
+        work_title: "Robofriends",
+        description: "Robofriends. A react application that lets you live search and filter from a directory of Robos using many of the react js cutting edge technology.",
+        site: "https://vanessander.github.io/robofriends/",
+        source: "https://github.com/vanessander/robofriends"
+    },
+    {
+        id: 6,
+        image_url: "https://alcatraz-bucket-demo.s3.eu-north-1.amazonaws.com/facebook_cover_photo_2.png",
+        work_title: "Background Generator",
+        description: " Make cool, free backgrounds quickly for your project with this easy to use tool. An app that allows the user to set the page's background color and get a color representation in real time",
+        site: "https://vanessander.github.io/background-generator/",
+        source: "https://github.com/vanessander/background-generator"
+    },
+    {
+        id: 7,
+        image_url: "https://alcatraz-bucket-demo.s3.eu-north-1.amazonaws.com/Screenshot+2024-09-06+at+20.22.22.jpg",
+        work_title: "Landing Page",
+        description: " A cool startup landing page for any idea that comes up. We never know! (:",
+        site: "https://vanessander.github.io/landingpage/",
+        source: "https://github.com/vanessander/landingpage"
+    },
+];
+
+export default data;
